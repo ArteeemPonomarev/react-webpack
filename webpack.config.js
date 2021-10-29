20731979383fbbc31b73b0081daa5329416e2f50
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-10-29 03:02:13 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[fullhash].js"
+    },
+    plugins: [
+        new HTMLWebpackPlugin({template: "./src/index.html"}),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|less)$/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)/,
+                use: ["file-loader"]
+            }
+        ],
+    }
+}
